7e5ede94f4657ced2ea4df04c590b1c4a5eb7879
NQ-OO <lol1219@snu.ac.kr>
2022-07-28 00:42:08 +0900

    [fix] fix order of userpdfs

--- src/model/pdfModel.js
+++ src/model/pdfModel.js
@@ -37,7 +37,7 @@ async function getUserPdfs(userId) {
             FROM userbooks
                     JOIN pdfs p on userbooks.pdfIdx = p.pdfIdx
             WHERE userbooks.userIdx = ${userId}
-            ORDER BY userbooks.updatedAt;
+            ORDER BY userbooks.updatedAt DESC ;
                   `;
   
     const [getUserPdfInfoRows] = await connection.query(
